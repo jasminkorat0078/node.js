95fe89cdc402844ba601c040753a4e68289150fe
jasmin <jasminkorat274200@gmail.com>
2022-05-22 12:14:15 +0530

    send response

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log("jasmin");
+    if(req.url ==='/'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<header><title> home page</title></header>');
+        res.write('<body> welcome home</body>');
+        res.write('</html>');
+        res.end();
+    }
+    if(req.url ==='/about'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<header><title> about page</title></header>');
+        res.write('<body> welcome about page</body>');
+        res.write('</html>');
+        res.end();
+    }
+    if(req.url ==='/node'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<header><title> Node.js page</title></header>');
+        res.write('<body> welcome Node.js page</body>');
+        res.write('</html>');
+        res.end();
+    }
+
+
 });
 
 server.listen(4000);
