c59e7df3301da06e88d98204cfe879c6686120fd
jasmin <jasminkorat274200@gmail.com>
2022-05-22 22:29:10 +0530

    parsing incomin req

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 
 const app = express();
-app.use((req,res,next)=>{
-    console.log("midleware1 is called");
-    next();
-});
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req,res,next)=>{
-    console.log("middleware2 is called");
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"> <input type="text" name="title"><button type="submit">add</button></form>');
+});
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use('/',(req,res,next)=>{
     res.send('<h1>hello node.js</h1>');
 })
 app.listen(3000);
