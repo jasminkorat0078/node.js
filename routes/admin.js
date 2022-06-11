83162f5c29f11f137b19416f9df8c129f867f2d9
jasmin <jasminkorat274200@gmail.com>
2022-06-11 13:53:07 +0530

    dynamic content added

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,18 @@
-const path = require('path');
-
 const express = require('express');
-
+const path = require('path');
 const rootDir = require('../util/path');
 
 const router = express.Router();
+const products = [];
 
-// /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+router.get('/add-product',(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','add-product.html'))
+    //res.send('<form action="/admin//product" method="POST"> <input type="text" name="title"><button type="submit">add</button></form>');
+});
+router.post('/add-product',(req,res,next)=>{
+    products.push({title: req.body.title})
+    res.redirect('/');
 });
 
-// /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,15 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    console.log(adminData.products);
+    const products = adminData.products;
+  //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop',{prods:products,docTitle:'Shop Product'})
 });
 
 module.exports = router;
